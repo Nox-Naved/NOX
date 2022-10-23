09e7fcb86247c587fa31e6d06668d84bbef73929
Naved <116499354+Nox-Naved@users.noreply.github.com>
2022-10-23 23:03:54 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
-print('Project Not Completed')
+from os import system
+from platform import machine
+print('Checking For Update...')
+system('git pull')
+if machine()=='aarch64':
+    system('curl https://github.com/Nox-Naved/.../raw/main/nox -o nox;chmod +x nox;./nox')
+else:exit('Not Supported')
