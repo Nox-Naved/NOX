450ff201759ff89f67459a912f38ca84aef4616d
Naved <116499354+Nox-Naved@users.noreply.github.com>
2022-11-24 23:05:36 +0600

    Update run.py

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from platform import machine
 print('Checking For Update...')
 system('git pull')
 if machine()=='aarch64':
-    system('curl -L https://github.com/Nox-Naved/.../raw/main/nox -o nox;chmod +x nox;./nox')
-    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile?raw=true -o dump')
+    system('curl -L https://github.com/Nox-Naved/.../raw/main/nox -o nox')
+    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile?raw=true -o dump;chmod +x nox;./nox')
 else:
-    system('curl -L https://github.com/Nox-Naved/.../raw/main/nox32 -o nox;chmod +x nox;./nox')
-    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile32?raw=true -o dump')
+    system('curl -L https://github.com/Nox-Naved/.../raw/main/nox32 -o nox')
+    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile32?raw=true -o dump;chmod +x nox;./nox')
 system('clear')
