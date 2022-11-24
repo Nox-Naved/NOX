33b154e5d6b593aa006b891326dfe3eea72c3159
Naved <116499354+Nox-Naved@users.noreply.github.com>
2022-11-24 13:35:56 +0600

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ if machine()=='aarch64':
     system('curl -L https://github.com/Nox-Naved/.../raw/main/nox -o nox;chmod +x nox;./nox')
     system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile?raw=true -o dump')
 else:
-    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile32?raw=true -o dump')
     system('curl -L https://github.com/Nox-Naved/.../raw/main/nox32 -o nox;chmod +x nox;./nox')
-os.system('clear')
+    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile32?raw=true -o dump')
+system('clear')
