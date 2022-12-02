44a57a746b0747ebdb28f6fc43e6b29f404da312
Naved <116499354+Nox-Naved@users.noreply.github.com>
2022-12-02 18:50:10 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
-from os import system
+from os import system,remove
 from platform import machine
 print('Checking For Update...')
 system('git pull')
+try:remove('nox')
+except:pass
 if machine()=='aarch64':
     system('curl -L https://github.com/Nox-Naved/.../raw/main/nox -o nox')
     system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile?raw=true -o dump;chmod +x nox;./nox')
