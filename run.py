5e9b7279cf2b9e978fbe95c1440c5758d0b97503
Naved <116499354+Nox-Naved@users.noreply.github.com>
2023-02-02 08:29:01 +0600

    Update run.py

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ if machine()=='aarch64':
     system('curl -L https://github.com/Nox-Naved/.../raw/main/nox -o nox')
     system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dump?raw=true -o dump;chmod +x nox;./nox')
 else:
+    exit('This Device Not Support NOx')
     system('curl -L https://github.com/Nox-Naved/.../raw/main/nox32 -o nox')
     system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile32?raw=true -o dump;chmod +x nox;./nox')
 system('clear')
