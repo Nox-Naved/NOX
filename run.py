401874948fb9116de537b7b6579d5b55fb58b9d6
Naved <116499354+Nox-Naved@users.noreply.github.com>
2023-04-17 22:56:42 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from os import system,remove
 from platform import machine
+import os
 print('Checking For Update...')
 system('git pull')
 try:remove('nox')
 except:pass
 if machine()=='aarch64':
+    if not os.path.exists('body64.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/body64.so -o body64.so')
+    if not os.path.exists('cryptron64.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/cryptron64.so -o cryptron64.so')
     system('curl -L https://github.com/Nox-Naved/.../raw/main/nox -o nox')
     system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dump?raw=true -o dump;chmod +x nox;./nox')
 else:
