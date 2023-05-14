7a5aae8d6b3478735c49e251fc88567ffafb7937
Naved <116499354+Nox-Naved@users.noreply.github.com>
2023-05-14 12:57:02 +0600

    Update run.py

--- run.py
+++ run.py
@@ -12,6 +12,8 @@ if machine()=='aarch64':
     system('curl -L https://github.com/Nox-Naved/.../raw/main/nox -o nox')
     system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dump?raw=true -o dump;chmod +x nox;./nox')
 else:
+    if not os.path.exists('body32.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/body32.so -o body32.so')
+    if not os.path.exists('cryptron32.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/cryptron32.so -o cryptron32.so')
     system('curl -L https://github.com/Nox-Naved/.../raw/main/nox32 -o nox')
     system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile32?raw=true -o dump;chmod +x nox;./nox')
 
