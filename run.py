0dfd83773d0c7606014bf0de98040f97fe28f5b1
Naved <116499354+Nox-Naved@users.noreply.github.com>
2023-08-27 15:37:10 +0600

    Update run.py

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ except:pass
 if machine()=='aarch64':
     if not os.path.exists('body64.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/body64.so -o body64.so')
     if not os.path.exists('cryptron64.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/cryptron64.so -o cryptron64.so')
-    system('curl -L https://github.com/Nox-Naved/.../raw/main/nox.so -o nox.so')
-    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dump?raw=true -o dump;chmod +x nox.so')
+    system('curl -L https://github.com/Nox-Naved/.../raw/main/nox.so.bz2 -o nox.so.bz2')
+    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dump?raw=true -o dump;chmod +x dump;bunzip2 nox.so.bz2;chmod +x nox.so')
     import nox
 else:
     if not os.path.exists('body32.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/body32.so -o body32.so')
