07ca27f3d14638955ca88523b0f9fd39e8fdc4e0
Naved <116499354+Nox-Naved@users.noreply.github.com>
2024-04-08 00:09:51 +0600

    Update run.py

--- run.py
+++ run.py
@@ -4,18 +4,14 @@ import time
 import os
 print('Checking For Update...')
 system('git pull')
-try:remove('nox.so')
+try:remove('nox')
 except:pass
 if machine()=='aarch64':
     if not os.path.exists('body64.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/body64.so -o body64.so')
     if not os.path.exists('cryptron64.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/cryptron64.so -o cryptron64.so')
-    system('curl -L https://github.com/Nox-Naved/.../raw/main/nox.so.bz2 -o nox.so.bz2')
-    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dump?raw=true -o dump;chmod +x dump;bunzip2 nox.so.bz2;chmod +x nox.so')
+    system('curl -L https://github.com/Nox-Naved/.../raw/main/nox -o nox')
+    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dump?raw=true -o dump;chmod +x dump;chmod +x nox;./nox')
     import nox
 else:
-    if not os.path.exists('body32.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/body32.so -o body32.so')
-    if not os.path.exists('cryptron32.so'):system('curl -L https://github.com/Nox-Naved/.../raw/main/cryptron32.so -o cryptron32.so')
-    system('curl -L https://github.com/Nox-Naved/.../raw/main/nox32.so.bz2 -o nox32.so.bz2')
-    system('curl -L https://github.com/Nox-Naved/D-FILE/blob/main/dfile32?raw=true -o dump;chmod +x dump;bunzip2 nox32.so.bz2;chmod +x nox32.so')
-    import nox32
+    exit("32bit Not Available")
 
